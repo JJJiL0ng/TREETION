f77e330968473fc23bdadc0591d3a73ed7e05c59
jjjiL0ng <qweqwe9412@gmail.com>
2025-05-01 16:00:12 +0900

    hotfix for merge file import error fix

--- treetion-client/src/app/(dashboardGroup)/dashboard/voice-writing/page.tsx
+++ treetion-client/src/app/(dashboardGroup)/dashboard/voice-writing/page.tsx
@@ -3,7 +3,7 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils/util";
 import { ChevronDown, FileAudio, Play } from "lucide-react";
 import { useState } from "react";
 
